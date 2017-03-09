75e418b9c95f2f4f88e5bd2ccdd376224dc397bb
UFuk <ufukcatalca@hotmail.com>
2017-03-09 23:06:56 +0300

    Done

--- PopUp/ViewController.swift
+++ PopUp/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func popUpAc(_ sender: Any) {
+      
+        let alertController = UIAlertController(title: "Hello Wordl", message: "This is message", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
 }
 
